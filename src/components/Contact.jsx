18ba515603dbc5c222d70f5cb13052f8d6ba3ac9
Pushti <pushti.ratanghayra@gmail.com>
2023-05-19 14:18:58 +0530

    links added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,6 @@
 import React, { useRef, useState } from "react";
 import { motion } from "framer-motion";
 import emailjs from "@emailjs/browser";
-import emailJs from 'emailjs-com';
 import { styles } from "../styles";
 import { EarthCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
--- src/components/Footers.jsx
+++ src/components/Footers.jsx
@@ -6,8 +6,8 @@ const Footer = () => {
     return (
         <div className='h-28'>
             <div className='flex justify-center align-middle mt-8 duration-200 transition-all ease-in-out'>
-            <CiLinkedin className='w-9 h-9 mr-4 cursor-pointer hover:scale-110' />
-            <FaGithub className='w-9 h-9 cursor-pointer hover:scale-110' />
+            <a href="https://www.linkedin.com/in/pushti-ratanghayra-1586a1212/"><CiLinkedin className='w-9 h-9 mr-4 cursor-pointer hover:scale-110' /></a> 
+            <a href="https://github.com/Pushti-R"><FaGithub className='w-9 h-9 cursor-pointer hover:scale-110' /> </a>
             </div>
             <p className='flex text-[#ffff] text-[14px] justify-center items-center mt-3'>Made with Passion by Pushti R.</p>
         </div>
